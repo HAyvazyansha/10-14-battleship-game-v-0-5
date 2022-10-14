b723f6ba97c3d6f39f232991a9aae122140f8afd
HAyvazyansha <113388054+HAyvazyansha@users.noreply.github.com>
2022-10-14 14:03:10 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,166 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    turnBoat(currentBoat)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    cursor.setFlag(SpriteFlag.Invisible, false)
+    moveBoatFlag = 0
+})
+function makeBoatVisble (boatArray: Sprite[]) {
+    for (let value of boatArray) {
+        value.setFlag(SpriteFlag.Invisible, false)
+    }
+}
+function moveBoat (boatArray: any[]) {
+    makeBoatVisble(boatArray)
+    if (grid.spriteRow(cursor) == 6 && boatRotateArray[currentBoat] == "up") {
+        grid.move(cursor, 0, -1)
+    }
+    if (grid.spriteCol(cursor) == 9 && boatRotateArray[currentBoat] == "sideways") {
+        grid.move(cursor, -1, 0)
+    }
+    cursor.setFlag(SpriteFlag.Invisible, true)
+    iterator = 0
+    for (let value of boatArray) {
+        if (boatRotateArray[currentBoat] == "up") {
+            grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor), grid.spriteRow(cursor) + iterator))
+        } else {
+            grid.place(value, tiles.getTileLocation(grid.spriteCol(cursor) + iterator, grid.spriteRow(cursor)))
+        }
+        iterator += 1
+    }
+}
+function makeBoatInvinsible (boatArray: Sprite[]) {
+    for (let value of boatArray) {
+        value.setFlag(SpriteFlag.Invisible, true)
+    }
+}
+function turnBoat (boatNum: number) {
+    if (boatRotateArray[boatNum] == "up") {
+        boatRotateArray[boatNum] = "sideways"
+    } else {
+        boatRotateArray[boatNum] = "up"
+    }
+}
+let iterator = 0
+let cursor: Sprite = null
+let moveBoatFlag = 0
+let boatRotateArray: string[] = []
+let currentBoat = 0
+currentBoat = 0
+boatRotateArray = ["up", "up", "up"]
+let boatSpriteArray = [[sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player)], [sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player)]]
+for (let value of boatSpriteArray) {
+    makeBoatInvinsible(value)
+}
+moveBoatFlag = 1
+tiles.setCurrentTilemap(tilemap`level2`)
+cursor = sprites.create(img`
+    3 3 3 3 3 3 . . . . 3 3 3 3 3 3 
+    3 3 3 3 3 . . . . . . 3 3 3 3 3 
+    3 3 3 3 . . . . . . . . 3 3 3 3 
+    3 3 3 . . . . . . . . . . 3 3 3 
+    3 3 . . . . . . . . . . . . 3 3 
+    3 . . . . . . . . . . . . . . 3 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    3 . . . . . . . . . . . . . . 3 
+    3 3 . . . . . . . . . . . . 3 3 
+    3 3 3 . . . . . . . . . . 3 3 3 
+    3 3 3 3 . . . . . . . . 3 3 3 3 
+    3 3 3 3 3 . . . . . . 3 3 3 3 3 
+    3 3 3 3 3 3 . . . . 3 3 3 3 3 3 
+    `, SpriteKind.Player)
+grid.snap(cursor)
+grid.moveWithButtons(cursor)
+game.onUpdate(function () {
+    if (moveBoatFlag == 1) {
+        moveBoat(boatSpriteArray[currentBoat])
+    }
+})
